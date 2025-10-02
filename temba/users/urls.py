f37eb6e109e07eee9c2d7996a61f79c1ce8a2e9a
lucaslinhares <lucaslinhares027@gmail.com>
2025-10-02 19:37:08 -0300

    get back with legacy user url

--- temba/users/urls.py
+++ temba/users/urls.py
@@ -5,4 +5,7 @@ from .views import TembaLoginView, TembaSignupView
 urlpatterns = [
     re_path(r"accounts/login", view=TembaLoginView.as_view(), name="account_login"),
     re_path(r"accounts/signup", view=TembaSignupView.as_view(), name="account_signup"),
+    # Legacy paths kept for backwards compatibility with old frontends/proxies
+    re_path(r"users/login/?$", view=TembaLoginView.as_view(), name="legacy_account_login"),
+    re_path(r"users/signup/?$", view=TembaSignupView.as_view(), name="legacy_account_signup"),
 ]
