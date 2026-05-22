760d39366f05da2d2e5a8bf3fdbe8e552fd7b12d
lucaslinhares <lucaslinhares027@gmail.com>
2026-05-22 01:27:04 -0300

    Resolve incosistence in flow editor version

--- temba/settings_common.py
+++ temba/settings_common.py
@@ -125,7 +125,7 @@ TESTFILES_DIR = os.path.join(PROJECT_DIR, "../testfiles")
 STATICFILES_DIRS = (
     os.path.join(PROJECT_DIR, "../static"),
     os.path.join(PROJECT_DIR, "../media"),
-    os.path.join(PROJECT_DIR, "../node_modules/@weni/flow-editor-rp/build"),
+    os.path.join(PROJECT_DIR, "../node_modules/@nyaruka/flow-editor/build"),
     os.path.join(PROJECT_DIR, "../node_modules/@nyaruka/temba-components/dist/static"),
     os.path.join(PROJECT_DIR, "../node_modules"),
     os.path.join(PROJECT_DIR, "../node_modules/react/umd"),
