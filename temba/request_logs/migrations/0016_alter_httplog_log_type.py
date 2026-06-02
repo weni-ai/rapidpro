07a7493d93d07599a19f44aeeac37ac94db7a0e6
lucaslinhares <lucaslinhares027@gmail.com>
2026-06-02 16:52:46 -0300

    Fix request logs migration dependency

--- temba/request_logs/migrations/0016_alter_httplog_log_type.py
+++ temba/request_logs/migrations/0016_alter_httplog_log_type.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("request_logs", "0014_squashed"),
+        ("request_logs", "0015_squashed"),
     ]
 
     operations = [
