eeeb64c4b55d90524901d38beb59c2ab5206707a
lucaslinhares <lucaslinhares027@gmail.com>
2026-07-07 15:18:20 -0300

    Run lint

--- temba/temba_celery.py
+++ temba/temba_celery.py
@@ -19,4 +19,6 @@ class TembaCelery(celery.Celery):
 app = TembaCelery("temba")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-app.autodiscover_tasks(("temba.channels.types.twitter", "temba.channels.types.whatsapp_legacy", "temba.channels.types.turn"))
+app.autodiscover_tasks(
+    ("temba.channels.types.twitter", "temba.channels.types.whatsapp_legacy", "temba.channels.types.turn")
+)
